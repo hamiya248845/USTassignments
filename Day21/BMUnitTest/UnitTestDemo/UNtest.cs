0a765d5cbd9daaa2eadbbba807ac766e6d42057a
hamiya248845 <129931550+hamiya248845@users.noreply.github.com>
2023-05-04 17:55:44 +0530

    Add files via upload

---
+++ Day21/BMUnitTest/UnitTestDemo/UNtest.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+
+using BMUnitTest;
+
+namespace UnitTestDemo
+
+{
+
+  [TestClass]
+
+  public class BMUnitTest1
+
+  {
+
+    [TestMethod]
+
+    public void Test_AddMethod()
+
+    {
+
+      BasicMath bm = new BasicMath();
+
+      double res = bm.Add(20, 10);
+
+      Assert.AreEqual(30, res);
+
+    }
+
+    [TestMethod]
+
+    public void Test_SubtractMethod()
+
+    {
+
+      BasicMath bm = new BasicMath();
+
+      double res = bm.Subtract(20, 10);
+
+      Assert.AreEqual(10, res);
+
+    }
+
+    [TestMethod]
+
+    public void Test_DivideMethod()
+
+    {
+
+      BasicMath bm = new BasicMath();
+
+      double res = bm.Divide(20 , 10);
+
+      Assert.AreEqual(2, res);
+
+    }
+
+    [TestMethod]
+
+    public void Test_MultiplyMethod()
+
+    {
+
+      BasicMath bm = new BasicMath();
+
+      double res = bm.Multiply(20, 10);
+
+      Assert.AreEqual(200, res);
+
+    }
+
+  }
+
+}
+
+
