6c6d43885b674814c24efd605dcc75d6df5c4f4b
hamiya248845 <129931550+hamiya248845@users.noreply.github.com>
2023-05-16 10:04:18 +0530

    Add files via upload

---
+++ Day28/CarDrillDebug/CarDrillDebug/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDrillDebug
+{
+    public class Product
+    { 
+        public long _id;
+        public string _supplierName;
+        public string _productName;
+    }
+}
