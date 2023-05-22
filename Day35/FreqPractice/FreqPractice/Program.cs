c29a4b62a6f4c306c047fe7b0200e7d831c25bd7
hamiya248845 <129931550+hamiya248845@users.noreply.github.com>
2023-05-22 14:13:55 +0530

    Add files via upload

---
+++ Day35/FreqPractice/FreqPractice/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreqPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size");
+            int n=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the elements");
+            int[] array=new int[n];
+            for(int i=0; i<n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Dictionary<int, int> freq = countFreq(array);
+            foreach (var pair in freq)
+            {
+                Console.WriteLine($"{pair.Key} occures {pair.Value}");
+            }
+            
+        }
+
+        private static Dictionary<int, int> countFreq(int[] array)
+        {
+            Dictionary<int,int> keyValuePairs = new Dictionary<int, int>();
+            foreach (var item in array)
+            {
+                if (keyValuePairs.ContainsKey(item))
+                {
+                    keyValuePairs[item]++;
+                }
+                else
+                {
+                    keyValuePairs[item] = 1;
+                }
+                
+            }
+            return keyValuePairs;
+        }
+    }
+}
---
+++ Day35/practice/practice/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter the size");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] arr = new int[n];
+            //for (int i = 0; i < n; i++) 
+            //{
+            //    arr[i] = int.Parse(Console.ReadLine());
+            //}
+            //foreach (int item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //for(int i= n-1; i>=0; i--)
+            // {
+            //  Console.WriteLine(arr[i]);
+            //}
+            //int sum = 0;
+            //for(int i = 0; i < n;i++)
+            //{
+            //    sum += arr[i];
+            //}
+            //Console.WriteLine(sum);
+
+            //int[] a= new int[n];
+            //for(int i = 0; i < n; i++)
+            //{
+            //    a[i] = arr[i];
+            //}
+            //foreach(int item in a)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //int count = 0;
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = i + 1; j < n; j++)
+            //    {
+
+            //        if (arr[i] == arr[j])
+            //        {
+            //            count++;
+            //        }
+
+            //    }
+            //}
+            //for (int i = 0; i < n; i++) {
+            //    if (count > 0) {
+            //        Console.WriteLine("no {0},frq {1}", arr[i],count);
+
+            //    }
+
+
+            //}
+
+            Console.WriteLine("enter the string");
+            string str = Console.ReadLine();
+            Console.WriteLine(str);
+
+        }
+    }
+    }
