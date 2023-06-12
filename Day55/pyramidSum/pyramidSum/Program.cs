f26a9f39b332abaa11c6c242098b5653e65ef198
hamiya248845 <129931550+hamiya248845@users.noreply.github.com>
2023-06-12 17:53:18 +0530

    Add files via upload

---
+++ Day55/pyramidSum/pyramidSum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System;
+
+class Solution
+{
+    static int pyramidSum(int array_length, List<int> arr)
+    {
+        // Write your code here
+        while (array_length > 1)
+        {
+            List<int> temp = new List<int>();
+            for (int i = 0; i < array_length - 1; i++)
+            {
+                int sum = arr[i] + arr[i + 1];
+                temp.Add(sum);
+            }
+            arr = temp;
+            array_length = temp.Count;
+        }
+        return arr[0];
+    }
+
+    static void Main(string[] args)
+    {
+        int array_length = Int32.Parse(Console.ReadLine().Trim());
+        List<int> arr = new List<int>();
+        for (int j = 0; j < array_length; j++)
+        {
+            arr.Add(Int32.Parse(Console.ReadLine().Trim()));
+        }
+        int result = pyramidSum(array_length, arr);
+        Console.WriteLine(result);
+    }
+}
+
---
+++ Day55/scoreString/scoreString/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+
+class Solution
+{
+    static int scoreString(String str)
+    {
+        // Write your code here
+        int score1 = 0, score2 = 0;
+        string substring = "";
+        string reverse = "";
+        for (int i = 0; i < str.Length - 4; i++)
+        {
+            substring = str.Substring(i, 5);
+            reverse = new string(substring.ToCharArray().Reverse().ToArray());
+            if (substring.Equals(reverse))
+            {
+                score1 += 10;
+            }
+        }
+        for (int j = 0; j < str.Length - 3; j++)
+        {
+            substring = str.Substring(j, 4);
+            reverse = new string(substring.ToCharArray().Reverse().ToArray());
+            if (substring.Equals(reverse))
+            {
+                score2 += 5;
+            }
+        }
+        return score1 + score2;
+    }
+
+    static void Main(string[] args)
+    {
+        string str;
+        str = Console.ReadLine();
+        int result;
+        result = scoreString(str);
+        Console.Write(result + "\n");
+    }
+}
